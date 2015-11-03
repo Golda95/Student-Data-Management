8e5952f021fa7286802727951abbe588c045879f
Golda95 <goldageorge.1995@gmail.com>
2015-11-03 12:13:26 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,15 @@
 /*Function prototype and structure declaration*/
-typedef struct student{
-	char *fname, *lname;
-	double MIS;
-	int year;
-	char *dept;
+#define MAX 500
+typedef struct student {
+	char fname[MAX], lname[MAX], ward[MAX], city[MAX], state[MAX], contact[11], pin[7];
+	int rno, dob, mob, yob, madmission, yadmission;
 	int sub1, sub2, sub3, sub4, sub5;
 }student;
-void addrec(char *argv[1]);
+
+void viewscore();
+void viewresult();
+//int studentlogin(char *sp);
+int studentmenu();
+//void adminlogin();
+int adminmenu();
+void addrecord(char *fn);
