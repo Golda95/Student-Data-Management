5d6eca7643429bb261183332b85ccce309e758d6
Golda95 <goldageorge.1995@gmail.com>
2015-11-10 13:18:27 +0530

    Update mainfuncs.c

--- mainfuncs.c
+++ mainfuncs.c
@@ -2,13 +2,205 @@
 #include<string.h>
 #include<stdlib.h>
 #include"main1.h"
+
+void sviewscore(int rno, char *fn) {
+	FILE *fp;
+	student s;
+	fp = fopen(fn, "r");
+	float net;
+	char *grade;
+	if(fp == NULL)
+		printf("Operation failed\n");
+	while(fread(&s, sizeof(s), 1, fp) == 1)
+		if(rno == s.rno) {
+			net = ((s.sub1 + s.sub2 + s.sub3 + s.sub4 + s.sub5) / 5) * 100;
+			if(net <= 100 && net >= 90)
+				grade = "AA";
+			else if(net < 90 && net >= 80)
+				grade = "AB";
+			else if(net < 80 && net >= 70)
+				grade = "BB";
+			else if(net < 70 && net >= 60)
+				grade = "BC";
+			else if( net < 60 && net >= 50)
+				grade = "CC";
+			else if(net < 50 && net >= 40)
+				grade = "CD";
+			else if(net < 40 && net >= 30)
+				grade = "DD";
+			else
+				grade = "FF";
+			printf("Sub 1 : %d"
+				"Sub 2 : %d"
+				"Sub 3 : %d"
+				"Sub 4 : %d"
+				"Sub 5 : %d"
+				"Percentage score : %f"
+				"Grade : %s", s.sub1, s.sub2, s.sub3, s.sub4, s.sub5, net, grade);
+			if(net >= 30)
+				printf("PASSED\n");
+		}
+}
+void viewall(char *fn) {
+	FILE *fp;
+	student s;
+	float net;
+	char *grade;
+	fp = fopen(fn, "r");
+	if(fp == NULL)
+		printf("Operation failed\n");
+	while(fread(&s, sizeof(s), 1, fp) == 1) {
+		net = ((s.sub1 + s.sub2 + s.sub3 + s.sub4 + s.sub5) / 5) * 100;
+		if(net <= 100 && net >= 90)
+			grade = "AA";
+		else if(net < 90 && net >= 80)
+			grade = "AB";
+		else if(net < 80 && net >= 70)
+			grade = "BB";
+		else if(net < 70 && net >= 60)
+			grade = "BC";
+		else if( net < 60 && net >= 50)
+			grade = "CC";
+		else if(net < 50 && net >= 40)
+			grade = "CD";
+		else if(net < 40 && net >= 30)
+			grade = "DD";
+		else
+			grade = "FF";
+		printf("First name : %s"
+			"Last name : %s	"
+			"DoB	   : %d/%d/%d"
+			"Admitted in: %d/%d"
+			"Address   : %s, %s, %s, %s"
+			"Scores in individual subjects-"
+			"Sub 1 : %d"
+			"Sub 2 : %d"
+			"Sub 3 : %d"
+			"Sub 4 : %d"
+			"Sub 5 : %d"
+			"Percentage score : %f"
+			"Grade : %s", s.fname, s.lname, s.dob, s.mob, s.yob, s.moa, s.yoa, s.ward, s.district, s.state, s.pin, s.sub1, s.sub2, s.sub3, s.sub4, s.sub5, net, grade);
+	}
+	fclose(fp);
+}
+	
+void viewrecord(int rno, char *fn) {
+	FILE *fp;
+	student s;
+	float net;
+	char *grade;
+	fp = fopen(fn, "r");
+	if(fp == NULL)
+		printf("operation failed\n");
+	while(fread(&s, sizeof(s), 1, fp) == 1)
+		if(rno == s.rno) {
+			net = ((s.sub1 + s.sub2 + s.sub3 + s.sub4 + s.sub5) / 5) * 100;
+			if(net <= 100 && net >= 90)
+				grade = "AA";
+			else if(net < 90 && net >= 80)
+				grade = "AB";
+			else if(net < 80 && net >= 70)
+				grade = "BB";
+			else if(net < 70 && net >= 60)
+				grade = "BC";
+			else if( net < 60 && net >= 50)
+				grade = "CC";
+			else if(net < 50 && net >= 40)
+				grade = "CD";
+			else if(net < 40 && net >= 30)
+				grade = "DD";
+			else
+				grade = "FF";
+			printf("First name : %s"
+				"Last name : %s	"
+				"DoB	   : %d/%d/%d"
+				"Admitted in: %d/%d"
+				"Address   : %s, %s, %s, %s"
+				"Scores in individual subjects-"
+				"Sub 1 : %d"
+				"Sub 2 : %d"
+				"Sub 3 : %d"
+				"Sub 4 : %d"
+				"Sub 5 : %d"
+				"Percentage score : %f"
+				"Grade : %s", s.fname, s.lname, s.dob, s.mob, s.yob, s.moa, s.yoa, s.ward, s.district, s.state, s.pin, s.sub1, s.sub2, s.sub3, s.sub4, s.sub5, net, grade);
+		}
+	fclose(fp);
+}
+
+void editlastname(int rno, char *fn) {/*use fseek and fputs*/
+	FILE *fp;
+	student s;
+	char *name;
+	int count = 0, flag = 0;
+	fp = fopen(fn, "r+");
+	if(fp == NULL) {
+		printf("Operation failed\n");
+		return;
+	}
+	printf("Enter new last name\n");
+	scanf("%s", name);
+	while(fread(&s, sizeof(s), 1, fp) == 1) {
+		count++;
+		if(rno == s.rno) {
+			flag = 1;
+			strcpy(s.lname, name);
+			break;
+		}
+	}
+	if(flag == 0)
+		printf("This roll number does not exist\n");
+	else {
+		fseek(fp, (count - 1) * sizeof(s), 0);
+		//fprintf(fp, "%s", name);
+		fseek(fp, 0, 0);
+		printf("%s : checking\n", s.lname);
+	}
+	fclose(fp);
+	//while(fread(&s, sizeof(s), 1, 
+}
+
+void editfirstname(int rno, char *fn) {/*use fseek and fputs*/
+	FILE *fp;
+	student s;
+	char *name;
+	int count = 0, flag = 0;
+	fp = fopen(fn, "w+");
+	if(fp == NULL) {
+		printf("Operation failed\n");
+		return;
+	}
+	printf("Enter new first name\n");
+	scanf("%s", name);
+	while(fread(&s, sizeof(s), 1, fp) == 1) {
+		count++;
+		if(rno == s.rno) {
+			flag = 1;
+			strcpy(s.fname, name);
+			break;
+		}
+	}
+	if(flag == 0)
+		printf("This roll number does not exist\n");
+	else {
+		fseek(fp, (count - 1) * sizeof(s), 0);
+		fprintf(fp, name, s.fname);
+		fseek(fp, 0, 0);
+		printf("%s : checking\n", s.fname);
+	}
+	fclose(fp);
+	//while(fread(&s, sizeof(s), 1, 
+}
+
 void addrecord(char *fn) {
 	FILE *fp;
 	student s;
-	int rno;
+	int rno, check;
 	fp = fopen(fn, "a");
-	if(fp == NULL)
+	if(fp == NULL) {
 		printf("Operation failed\n");
+		return;
+	}
 	printf("Enter roll number\n");
 	scanf("%d", &rno);
 	if(rno == 0) {
@@ -17,11 +209,14 @@ void addrecord(char *fn) {
 			return;
 		}
 	while(fread(&s, sizeof(s), 1, fp) == 1)
-		if(rno == s.rno) {
+		if(s.rno == rno) {
+		/*fscanf(fp, "%d", &check);
+		if(check == rno) {*/
 			printf("This record already exists\n");
 			fclose(fp);
 			return;
-		}
+		//}
+	}
 	s.rno = rno;
 	printf("Enter student details\nFirst name :");
 	scanf("%s", s.fname);
@@ -34,14 +229,14 @@ void addrecord(char *fn) {
 	printf("yy :");
   	scanf("%d", &s.yob);
 	printf("Month of admission :");
-	scanf("%d", &s.madmission);
+	scanf("%d", &s.moa);
 	printf("Year :");
-	scanf("%d", &s.yadmission);
+	scanf("%d", &s.yoa);
   	printf("Address details(ward name) :");
 	scanf("%s", s.ward);
 	printf("City :");
-	scanf("%s", s.city);
-	printf("State :");
+	scanf("%s", s.district);
+	printf("State(Abbrev) :");
 	scanf("%s", s.state);
 	printf("PIN code :");
 	scanf("%s", s.pin);
@@ -56,19 +251,19 @@ void addrecord(char *fn) {
 	scanf("%d", &s.sub4);
 	printf("Sub 5 :");
 	scanf("%d", &s.sub5);
-	fprintf(fp, "%d  %s %s\t%d/%d/%d\t%d/%d\t%s\t%s, %s, %s- %s\t\t\t%d\t%d\t%d\t%d\t%d\n", s.rno, s.fname, s.lname, s.dob, s.mob, s.yob, s.madmission, s.yadmission, s.contact, s.locality, s.city, s.pin, s.state, s.sub1, s.sub2, s.sub3, s.sub4, s.sub5);
+	fprintf(fp, "%d  %s %s\t%d/%d/%d\t%d/%d\t%s\t%s, %s, %s- %s\t\t\t%d\t%d\t%d\t%d\t%d\n", s.rno, s.fname, s.lname, s.dob, s.mob, s.yob, s.moa, s.yoa, s.contact, s.ward, s.district, s.pin, s.state, s.sub1, s.sub2, s.sub3, s.sub4, s.sub5);
 	fclose(fp);
 	printf("Record added\n");
 	return;
 }
 
-void viewscore() {
+/*void viewscore() {
 	printf("Checking viewscore\n");
 }
 
 void viewresult() {
 	printf("Checking viewresult\n");
-}
+}*/
 
 int adminmenu() {
 	int choice;
