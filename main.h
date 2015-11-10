b409b52171fb34ed4029cbbac0aefa63da3690ce
Golda95 <goldageorge.1995@gmail.com>
2015-11-10 13:17:48 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,16 @@
 /*Function prototype and structure declaration*/
 #define MAX 500
 typedef struct student {
-	char fname[MAX], lname[MAX], ward[MAX], city[MAX], state[MAX], contact[11], pin[7];
-	int rno, dob, mob, yob, madmission, yadmission;
+	char fname[MAX], lname[MAX], ward[MAX], district[MAX], state[MAX], contact[11], pin[7];
+	int rno, dob, mob, yob, moa, yoa;
 	int sub1, sub2, sub3, sub4, sub5;
 }student;
 
-void viewscore();
-void viewresult();
-//int studentlogin(char *sp);
-int studentmenu();
-//void adminlogin();
-int adminmenu();
+void sviewscore(int rno, char *fn);
+void viewall(char *fn);
+void viewrecord(int rno, char *fn);
+void editlastname(int rno, char *fn);
+void editfirstname(int rno, char *fn);
 void addrecord(char *fn);
+int adminmenu();
+int studentmenu();
